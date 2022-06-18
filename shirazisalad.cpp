c585b256dd4523972c195b4c2f4206143df13c23
Mohammad Javad Bagheri Nejad <git@mjbn.ir>
2022-06-18 21:34:07 +0430

    using vector instead of array

--- shirazisalad.cpp
+++ shirazisalad.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc , char** arcv){
 	string text = "<h1>MJBN</h1>";
 	shirazisalad sh = shirazisalad(text);
-	cout << sh.htmltags << endl;
-	sh.printa();
+	//cout << sh.htmltags << endl;
+	//cout << sh.htmltags[0] << endl;
 	return 0;
 }
--- shirazisalad.hpp
+++ shirazisalad.hpp
@@ -1,52 +1,62 @@
 #ifndef shiraz_header
 #define shiraz_header
 
-#include <cstdio>
-#include <cstdlib>
-#include <iterator>
-#include <ostream>
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class shirazisalad {
 	private:
-	public:
-		string* htmltags;
-		int tagps;
-		shirazisalad(string html){
-			parse_html(html);
-		}
-		void parse_html(string rawdata){
+		int pre_parse(string rawdata){
+			int tagcount = 0;
 			int i = 0;
 			int n = rawdata.length();
-			string htmltag[n];
-			int tagpos = 0;
-			while (i<n) {
+			while (i<=n) {
 				int s = rawdata.find('<', i);
-				cout << s << endl;
+				if (s == -1){break;}
+				if(rawdata[s+1]!='!' and rawdata[s+1]!='/' and rawdata[s+1]!='s' and rawdata[s+1]!='S'){
+					tagcount++;
+				}
+				i = s+1;
+			}
+			return tagcount;
+		}
+		vector<string> parse_html(string rawdata){
+			int tagcount = pre_parse(rawdata);
+			int i = 0;
+			int n = rawdata.length();
+			vector<string> htmltag;
+			tagpos = 0;
+			while (i<n) {
+				cout << "loop start:"<<endl;
+				int s = rawdata.find('<', i);
 				int e = rawdata.find('>', s+1);
 				int endtag = rawdata.find("</", e+1);
-				if(rawdata[s+1]!='!' or rawdata[s+1]!='/' or rawdata[s+1]!='s' or rawdata[s+1]!='S'){
-					for (int i = s;i<=e;i++) {htmltag[tagpos] += rawdata[i];}
-					tagpos++;
-					for (int i = e+1; i<=endtag; i++) {htmltag[tagpos] += rawdata[i];}
-					tagpos++;
+				cout << s << endl;
+				cout << e << endl;
+				cout << endtag << endl;
+				cout << rawdata[s+1]<<endl;
+				if(rawdata[s+1]!='!' and rawdata[s+1]!='/' and rawdata[s+1]!='s' and rawdata[s+1]!='S'){
+					string tmp;
+					for (int j = s;j<=e;j++) {tmp += rawdata[j];}
+					cout << tmp<<endl;
+					htmltag.push_back(tmp);
+					cout << rawdata[endtag+1]<<endl;
+					if (rawdata[endtag+2]==rawdata[s+1]){
+						tmp = "";
+						for (int j = e+1; j<endtag; j++) {tmp += rawdata[j];}
+						cout << tmp <<endl;
+						htmltag.push_back(tmp);
+					}
 				}
 				i = e+1;
 			}
-			tagps = tagpos;
-			string htmltg[tagpos];
-			for(int i = 0; i<=tagpos; i++){
-				htmltg[i] = htmltag[i];
-			}
-			htmltags = htmltg;
+			return htmltag;
 		} 
-		void printa(){
-			for (int i = 0; i<=tagps; i++) {
-				cout << htmltags[0] << endl;
-			}
+	public:
+		shirazisalad(string html, string tag, string tagarg, string tagargvalue){
+			parse_html(html);
 		}
-
 };
 #endif
